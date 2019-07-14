5b3c9e85a3952cc6e006f41af94e14433f27858a
sujaany <ssujaany09@gmail.com>
2019-07-14 20:12:01 +0530

    strted with api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,53 +1,6 @@
 
 import { FETCH_RENTALS, FETCH_RENTAL_BY_ID_SUCCESS, FETCH_RENTAL_BY_ID_INIT } from './types';
 
-const rentals = [
-    {
-        id: "1",
-        title: "Nice view on ocean",
-        city: "Jaffna",
-        street: "Thirunelvely",
-        catergory: "condo",
-        image: "http://via.placeholder.com/350x250",
-        bedrooms: 4,
-        description: "very nice apartment",
-        dailyRate: 43
-    },
-    {
-        id: "2",
-        title: "Nice house",
-        city: "Vavuniya",
-        street: "School street",
-        catergory: "house",
-        image: "http://via.placeholder.com/350x250",
-        bedrooms: 5,
-        description: "very nice apartment",
-        dailyRate: 43
-    },
-    {
-        id: "3",
-        title: "Nice Place",
-        city: "Jaffna",
-        street: "School street",
-        catergory: "house",
-        image: "http://via.placeholder.com/350x250",
-        bedrooms: 5,
-        description: "very nice apartment",
-        dailyRate: 43
-    },
-    {
-        id: "3",
-        title: "Nice Place",
-        city: "Jaffna",
-        street: "School street",
-        catergory: "apartment",
-        image: "http://via.placeholder.com/350x250",
-        bedrooms: 5,
-        description: "very nice apartment",
-        dailyRate: 43
-    }
-];
-
 const fetchRentalByIdInit = () => {
     return {
         type: FETCH_RENTAL_BY_ID_INIT
